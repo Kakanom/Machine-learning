5ebcc96164e52a7d7acbe1683bdcf66a9607b9ea
Mark Smirnov <104085253+Kakanom@users.noreply.github.com>
2022-04-20 19:54:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import math_func
+
+a = float(input("Укажите, чему равно ребро куба "))
+print("Объем куба равен ", math_func.cube(a))
+
+a, b = input("Укажите катеты прямоуг. треуг. через пробел ").strip().split()
+print("Гипотенуза ", math_func.hipot(float(a), float(b)))
+
+a, b, c = map(float, input().split())
+
+
+
