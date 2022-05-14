152e5eaf26150ba027d53d034a68a1b34d866696
kakanom <marksmirnov15@mail.ru>
2022-05-14 17:23:06 +0300

    beta version 0.7 - some README changes, Kmeans model added

--- debugging.py
+++ debugging.py
@@ -2,7 +2,8 @@ from math_func import *
 from visualization import *
 from data_preprocessing import *
 from metrics import *
-from models import LinReg
+from models import *
+from sklearn.datasets import make_blobs
 import numpy as np
 #
 # draw_funcs(["x", "x ** 2", "x ** 3 + 2"], -10, 10, 100)
@@ -17,10 +18,17 @@ import numpy as np
 # # draw_poly_class(X, Y)
 # #
 #
-x = np.array([np.arange(0.0, 6.0, 0.5)]).T
-y = np.array([ 7.32808219,  8.88210587,  8.92239811, 10.52462399, 12.91597313,
-            12.55843474, 14.81755614, 18.31825374, 19.44109095, 18.76040251,
-            20.07294082, 22.10215284])
+# x = np.array([np.arange(0.0, 6.0, 0.5)]).T
+# y = np.array([ 7.32808219,  8.88210587,  8.92239811, 10.52462399, 12.91597313,
+#             12.55843474, 14.81755614, 18.31825374, 19.44109095, 18.76040251,
+#             20.07294082, 22.10215284])
+#
+# mod = LinReg()
+# mod.fit(x, y)
+#
+# draw_lin(x, y, mod.w)
+#
+# plt.show()
 #
 # # print(x, y, sep="\n")
 #
@@ -28,39 +36,23 @@ y = np.array([ 7.32808219,  8.88210587,  8.92239811, 10.52462399, 12.91597313,
 #                  [2],
 #                  [3]]), np.array([1, 2, 3])
 #
-# mod = LinReg()
-#
-# mod.fit(x, y)
-#
-# print(mod.predict(x))
-# print(mod.log)
-#
-# print(mod.score(x, y))
 #
 #
 # x = np.array([np.arange(0.0, 6.0, 0.5),
 #              np.arange(5.0, 6.11, 0.1)])
 # x = np.insert(x, 0, 0, axis = 0).T
 # w = np.array([[1., 1., 1.]]).T
-# #
-# # print(x)
-# # print(w)
-# #
-# # print(np.dot(x, w))
-#
-# print(polinom(np.array([1, 2, 3]), np.array([1, 2, 3])))
+
 
 # x, y = np.array([[1],
 #                  [2],
 #                  [3]]), np.array([1, 2, 3])
 #
-mod = LinReg()
 
-mod.fit(x, y)
+X, labs = make_blobs(1000, centers = 3)
 
-print(mod.score(x, y))
-print(mod.log)
+mod = Kmeans(n_clusters=3)
+lab = mod.clusterize(X)
 
-draw_lin(x, y, mod.w)
-
+draw_poly_class(X, lab)
 plt.show()
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 """Модуль, содержащий в себе некоторые модели машинного обучения"""
 from visualization import *
-
+from sklearn.metrics.pairwise import nan_euclidean_distances
 
 # linear regression
 
@@ -80,136 +80,99 @@ class LinReg:
         return MSE(y_true, y_pred)
 
 
-# class FOREL:
-#     def __init__(self, x, r):
-#         self.x = x
-#         self.r = r
-#         self.clusters = None
-#
-#     def find_centroid(self, x, c):
-#         """
-#         Принимает на вход список объектов x и список столбцов c, по которым ищется
-#         центр тяжести. Возвращает центр тяжести
-#         """
-#
-#         return x[c].mean(axis=0)
-#
-#     def find_cluster(self, x, f):
-#         """
-#         f - координаты случайной точки
-#         """
-#
-#         c = [i for i in range(x.shape[0]) if nan_euclidean_distances([X[i, :]], [f]) <= R]
-#         f = self.find_centroid(x, c)
-#         c1 = [i for i in range(x.shape[0]) if nan_euclidean_distances([X[i, :]], [f]) <= R]
-#         while c != c1:
-#             f = self.find_centroid(x, c)
-#             c = c1
-#             c1 = [i for i in range(x.shape[0]) if nan_euclidean_distances([X[i, :]], [f]) <= R]
-#
-#         return c
-#
-#     def find_all_clusters(self):
-#         cl = []
-#         iter = 0
-#         while self.x.shape[0] > 0 and iter < 10:
-#             f = np.random.randint(self.x.max(), size=2)
-#             cl += [self.find_cluster(x, self.r, f)]
-#             x = np.delete(x, cl, axis=0)
-#             iter += 1
-#
-#         return cl
-#
-#     def fit(self):
-#         self.clusters = self.find_all_clusters()
-#
-#     def visualization(self):
-#         draw_poly_class(self.x, self.clusters)
+class Kmeans:
+    """Класс кластеризации алгоритмом k-средних"""
+
+    def __init__(self, n_clusters, max_iter=100):
+        """
+
+        :param n_clusters:
+        :param max_iter:
+        """
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.centroids = None
 
 
-# class Kmeans:
-#     """Класс кластеризации алгоритмом k средних"""
-#
-#     def __init__(self, n_clusters, max_iter=100):
-#         self.n_clusters = n_clusters
-#         self.max_iter = max_iter
-#
-#
-#     def find_labs(self, x, centroids):
-#         dist = nan_euclidean_distances(x, centroids)
-#         return np.argmin(dist, axis = 1)
-#
-#
-#     def find_eucl_sum(self, x, point):
-#         return nan_euclidean_distances(x, [point]).sum(axis = 1)
-#
-#
-#     def find_centroids(self, x, labs):
-#       return np.array([x[labs == i].mean(axis = 0) for i in np.unique(labs)])
-#
-#
-#     def find_closest_cluster(self, distance):
-#         return np.argmin(distance, axis=1)
-#
-#
-#     def fit(self, x, k):
-#         if k < 2: return np.zeros((x.shape[0], 1))
-#
-#         centroids = np.random.normal(loc=0.0, scale=5., size=k * X.shape[1]). \
-#             reshape((k, x.shape[1]))
-#
-#
-#         labs = self.find_labs(x, centroids)
-#
-#         centroids1 = self.find_centroids(x, labs)
-#
-#         while abs(centroids - centroids1).max() > 1e-2:
-#             labs = self.find_labs(x, centroids1)
-#
-#             centroids = centroids1.copy()
-#             centroids1 = self.find_centroids(x, labs)
-#
-#         return labs
-#
-#     def predict(self, x):
+    def find_labs(self, x, centroids):
+        """классифицирует данные для текущих центроидов
+
+        :param x: признаки объектов
+        :param centroids: расположение центроидов
+        :return: метки классов для объектов
+        """
+        dist = nan_euclidean_distances(x, centroids)
+        return np.argmin(dist, axis = 1)
 
 
-# class LinearReg:
-#     """Класс линейной регрессии"""
-#     def __init__(self, learning_rate=1e-3, n_steps=1000):
-#         self.learning_rate = learning_rate
-#         self.n_steps = n_steps
-#
-#     def fit(self, X, Y):
-#         '''
-#         Method for learning the optimal parameters of the model
-#         '''
-#
-#         # adding the bias term
-#         Xtrain = np.c_[np.ones(X.shape[0]), X]
-#
-#         # random initialization of the model weights
-#         self.W = np.random.rand((Xtrain.shape[1]))
-#
-#         # iteratively updating W for n_steps
-#         for i in range(self.n_steps):
-#             self.W = self.W - self.learning_rate * self.calcGradient(Xtrain, Y)
-#
-#     def predict(self, X):
-#         '''
-#         Predicting Y for the X
-#         '''
-#
-#         # adding the bias term
-#         Xpred = np.c_[np.ones(X.shape[0]), X]
-#
-#         return np.dot(Xpred, self.W)
-#
-#     def calcGradient(self, X, Y):
-#         '''
-#         Implementation of the gradient of the Loss function
-#         '''
-#         return 2 / X.shape[0] * np.dot(X.T, (np.dot(X, self.W) - Y))
+    def find_eucl_sum(self, x, point):
+        return nan_euclidean_distances(x, [point]).sum(axis = 1)
+
+
+    def find_centroids(self, x, labs):
+      return np.array([x[labs == i].mean(axis = 0) for i in np.unique(labs)])
+
+
+    def find_closest_cluster(self, distance):
+        return np.argmin(distance, axis=1)
+
+
+    def clusterize(self, x, eps = 1e-3, tries = 100):
+        """кластеризует данные на n кластеров.
+
+        :param x: признаки объектов
+        :param eps: изминение расположения весов для того, чтобы остановить кластеризацию
+        :param tries: количество попыток запуска(из-за случайной генерации может получиться так, что какой-то кластер
+                                                                                                останется без объектов)
+        :return: метки классов для данных объектов(возвращает None, если не удалось кластеризовать, возможно,
+                                                                                                стоит запустить еще раз)
+
+        если вам нужно определить метки для других объектов по уже обученной модели, то используйте метод find_labs,
+        указав центроиды как instance_name.centroids
+        """
+        for i in range(tries):
+            try:
+                if self.n_clusters < 2:
+                    return np.zeros((x.shape[0], 1))
+
+                centroids = np.random.normal(loc=0.0, scale=5., size=self.n_clusters * x.shape[1]). \
+                    reshape((self.n_clusters, x.shape[1]))
+
+                labs = self.find_labs(x, centroids)
+
+                centroids1 = self.find_centroids(x, labs)
+
+                while abs(centroids - centroids1).max() > eps:
+                    labs = self.find_labs(x, centroids1)
+
+                    centroids = centroids1.copy()
+                    centroids1 = self.find_centroids(x, labs)
+
+                if len(np.unique(labs)) < self.n_clusters:
+                    continue
+
+                self.centroids = centroids1
+
+                return labs
+
+            except Exception:
+                continue
+
+        return None
+
+    def quality_of_clustering(self, x, labs):
+        """Метод, возвращающий качество кластеризации
+
+        :param x: признаки объектов
+        :param labs: их метки классов
+        :return: качество кластеризации(усредненная сумма растояний до центров)
+        """
+        S = 0
+
+        for i in np.unique(labs):
+            S += (self.find_eucl_sum(x[labs == i], x[labs == i].mean(axis=0))).sum()
+
+        return S
 
 
 if __name__ == '__main__':
