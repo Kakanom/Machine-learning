89d8085188a2642323c9c7da7e1254863664d86b
kakanom <marksmirnov15@mail.ru>
2022-08-28 15:39:27 +0300

    version 1 - packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+# Package meta-data.
+setup(
+    name = 'Kakan',
+    description = 'machine learning lib',
+    url = 'https://github.com/Kakanom/Machine-learning',
+    email = 'frostikoil@gmail.com',
+    author = 'Kakanom',
+    requires_python = '>=3.6.0',
+    version = '1.0.0',
+    install_requires = ['numpy>=1.20.3',
+                        'matplotlib>=3.4.3',
+                        'pandas>=1.3.4',
+                        'scikit_learn>=1.0.2'],
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts':[
+            'src-cli = src.main:main'
+        ],
+    },
+)
