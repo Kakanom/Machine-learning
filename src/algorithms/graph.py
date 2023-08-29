7b181d91a1f303646a4b137fe25c547a1761f7be
marksmirnov <frostikoil@gmail.com>
2023-08-29 17:59:48 +0300

    version 1.1.3' fixed dijkstra

--- src/algorithms/graph.py
+++ src/algorithms/graph.py
@@ -1,30 +1,31 @@
 from collections import deque
 
-# dijkstra's algorithm using heap (includes path)
+# dijkstra's algorithm using heap (includes path it's fucking useless)
 
 
 class MinHeap:
     def __init__(self):
-        self.heap = [[0, '']]
+        self.heap = [0]
         self.size = 0
+        self.dists = []
 
     def siftup(self, ind):
         while (ind >> 1) > 0:
-            if self.heap[ind][0] < self.heap[ind >> 1][0]:
-                self.heap[ind >> 1][0], self.heap[ind] = self.heap[ind][0], self.heap[ind >> 1][0]
+            if self.dists[self.heap[ind]] < self.dists[self.heap[ind >> 1]]:
+                self.heap[ind >> 1], self.heap[ind] = self.heap[ind], self.heap[ind >> 1]
 
             ind >>= 1
 
-    def insert(self, k, node):
-        self.heap.append([k, node])
+    def insert(self, node):
+        self.heap.append(node)
         self.size = self.size + 1
         self.siftup(self.size)
 
     def siftdown(self, ind):
         while (ind << 1) <= self.size:
             mc = self.min_child(ind)
-            if self.heap[ind][0] > self.heap[mc][0]:
-                self.heap[ind][0], self.heap[mc][0] = self.heap[mc][0], self.heap[ind][0]
+            if self.dists[self.heap[ind]] > self.dists[self.heap[mc]]:
+                self.heap[ind], self.heap[mc] = self.heap[mc], self.heap[ind]
 
             ind = mc
 
@@ -32,7 +33,7 @@ class MinHeap:
         if (ind << 1) >= self.size:
             return ind << 1
         else:
-            if self.heap[ind << 1][0] < self.heap[ind << 1 | 1][0]:
+            if self.dists[self.heap[ind << 1]] < self.dists[self.heap[ind << 1 | 1]]:
                 return ind << 1
             else:
                 return ind << 1 | 1
@@ -41,7 +42,7 @@ class MinHeap:
         if self.size == 0:
             return None
 
-        node = self.heap[1][1]
+        node = self.heap[1]
 
         if self.size == 1:
             self.heap.pop()
@@ -62,6 +63,9 @@ class MinHeap:
             self.siftdown(ind)
             ind = ind - 1
 
+    def setup(self, dists):
+        self.dists = dists
+
 
 def dijkstra(graph: dict, start, end, path=False):
     """
@@ -71,15 +75,14 @@ def dijkstra(graph: dict, start, end, path=False):
     :param path: True -> returns distance, path
                  False -> only distance
     """
-
-    dists = {el: float('inf') for el in graph.keys()}
+    dists = {el: float('inf') for el in graph.keys()}  # (dists now in the heap)
     visited = {el: False for el in graph.keys()}
     parents = {el: None for el in graph.keys()}
 
-    dists[start] = 0
-
     heap = MinHeap()
-    heap.build([[0, start]])
+    heap.build([start])
+    heap.setup(dists)
+    heap.dists[start] = 0
 
     while heap.size != 0:
         node = heap.pop()
@@ -88,11 +91,11 @@ def dijkstra(graph: dict, start, end, path=False):
             continue
 
         for v in graph[node].keys():
-            if dists[v] > dists[node] + graph[node][v]:
-                dists[v] = dists[node] + graph[node][v]
+            if heap.dists[v] > heap.dists[node] + graph[node][v]:
+                heap.dists[v] = heap.dists[node] + graph[node][v]
                 parents[v] = node
 
-            heap.insert(dists[v], v)
+                heap.insert(v)
 
         visited[node] = True
 
@@ -105,5 +108,8 @@ def dijkstra(graph: dict, start, end, path=False):
             cur = parents[cur]
 
         return dists[end], path[::-1]
+
+    if dists[end] == float('inf'):
+        return -1
 
     return dists[end]
